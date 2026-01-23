4fe9269598a26012455ec39fc78df4287c448437
madobeno <jittersquatstore@gmail.com>
2026-01-23 21:43:56 +0900

    feat

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@fontsource/noto-serif-jp/200.css'
+import '@fontsource/noto-serif-jp/400.css'
+import '@fontsource/noto-serif-jp/700.css'
 import './index.css'
 import App from './App.tsx'
 
